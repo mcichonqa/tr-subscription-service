d343ec5a7f166592f4d0ec2e0e3b2b9f196dc784
mcichonqa <michal.cichon.qa@gmail.com>
2023-04-16 20:30:22 +0100

    Adding subscriptionpackagedto model in controller

--- SubscriptionService.Api/Controllers/SubscriptionPackageController.cs
+++ SubscriptionService.Api/Controllers/SubscriptionPackageController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SubscriptionService.Contract;
 using SubscriptionService.Package;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SubscriptionService.Api.Controllers
@@ -19,12 +21,32 @@ namespace SubscriptionService.Api.Controllers
         [HttpGet("packages")]
         public IActionResult GetPackages()
         {
+            List<SubscriptionPackageDto> subPackages = new List<SubscriptionPackageDto>();
+
             var packages = _packageProvider.Packages;
 
             if (!packages.Any())
                 throw new Exception("Package could not be empty after deserialize.");
 
-            return Ok(packages);
+            packages.ForEach(package =>
+            {
+                var subPackage = new SubscriptionPackageDto()
+                {
+                    Name = package.Name,
+                    Duration = DateTime.UtcNow.AddMonths(package.Duration),
+                    Details = package.Details,
+                    Price = new PriceDto()
+                    {
+                        GrossValue = package.Price.GrossValue,
+                        NetValue = package.Price.NetValue,
+                        TotalValue = package.Price.TotalValue
+                    }
+                };
+
+                subPackages.Add(subPackage);
+            });
+
+            return Ok(subPackages);
         }
     }
 }
